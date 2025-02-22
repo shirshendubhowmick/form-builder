014e020240081d4443071a5b1ff11219a08569f0
Shirshendu Bhowmick <12836504+shirshendubhowmick@users.noreply.github.com>
2025-02-22 23:57:42 +0530

    integrate Builder component into App layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Builder from "./views/Builder/Builder";
+
 function App() {
-  return <main className="mx-auto max-w-screen-xl p-6">App</main>;
+  return (
+    <main className="mx-auto flex max-w-screen-xl flex-col p-6">
+      <Builder />
+    </main>
+  );
 }
 
 export default App;
