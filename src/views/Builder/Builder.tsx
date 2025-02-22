46fbe44f9287c3e3feb0604e2ee62d196f2d6467
Shirshendu Bhowmick <12836504+shirshendubhowmick@users.noreply.github.com>
2025-02-22 20:13:56 +0530

    add Builder component to implement form builder interface

---
+++ src/views/Builder/Builder.tsx
@@ -0,0 +1,14 @@
+import Form from "./components/Form/Form";
+
+function Builder() {
+  return (
+    <div>
+      <h1 className="mb-8">Form builder</h1>
+      <div>
+        <Form />
+      </div>
+    </div>
+  );
+}
+
+export default Builder;
