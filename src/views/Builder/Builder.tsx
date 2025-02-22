f1c28c307ea347ce96bf59b947a853b58795a0b5
Shirshendu Bhowmick <12836504+shirshendubhowmick@users.noreply.github.com>
2025-02-22 21:25:00 +0530

    implement dynamic input handling in Builder component with add more functionality

--- src/views/Builder/Builder.tsx
+++ src/views/Builder/Builder.tsx
@@ -1,11 +1,45 @@
+import { useCallback, useState } from "react";
+
+import Button, { COLOR, INTENT } from "~/components/Button/Button";
+import { BuilderFormData } from "~/schemas/builder";
+
 import Form from "./components/Form/Form";
 
 function Builder() {
+  const [inputs, setInputs] = useState<BuilderFormData[]>([]);
+  const [showBuilderForm, setShowBuilderForm] = useState(false);
+
+  const onSuccessfulAdd = useCallback((data: BuilderFormData) => {
+    setInputs((prev) => [...prev, data]);
+  }, []);
+
+  const onAddMore = useCallback(() => {
+    setShowBuilderForm(true);
+  }, []);
+
   return (
     <div>
       <h1 className="mb-8">Form builder</h1>
       <div>
-        <Form />
+        {inputs.map((input, index) => (
+          // eslint-disable-next-line react/no-array-index-key
+          <div key={index}>
+            <pre>{JSON.stringify(input, null, 2)}</pre>
+          </div>
+        ))}
+
+        {Boolean(inputs.length) && !showBuilderForm && (
+          <Button
+            color={COLOR.primary}
+            intent={INTENT.primary}
+            onClick={onAddMore}
+          >
+            Add more
+          </Button>
+        )}
+        {(!inputs.length || showBuilderForm) && (
+          <Form onSuccessfulAdd={onSuccessfulAdd} />
+        )}
       </div>
     </div>
   );
