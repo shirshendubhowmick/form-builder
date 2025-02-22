b9511444ad945115e26509681c872948faf0d273
Shirshendu Bhowmick <12836504+shirshendubhowmick@users.noreply.github.com>
2025-02-22 15:56:13 +0530

    add @types/node dependency and configure path alias in Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import react from "@vitejs/plugin-react";
+import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
@@ -6,5 +7,10 @@ export default defineConfig({
   plugins: [react({ include: /\.(ts|tsx)$/ })],
   server: {
     port: 3000,
+  },
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "./src"),
+    },
   },
 });
