cc7e0b20b072c1b401850cf4bc604ea89fdce839
Shirshendu Bhowmick <12836504+shirshendubhowmick@users.noreply.github.com>
2025-02-24 19:44:31 +0530

    feat: integrate draft entry management in App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,10 @@ import Button, { COLOR, INTENT, SIZE } from "./components/Button/Button";
 import { BuilderFormData } from "./schemas/builder";
 import {
   addSchema,
+  createDraftEntry,
+  deleteDraftEntry,
   deleteQuestionFromSchema,
+  getDraftEntry,
   getSchemas,
   updateSchema,
 } from "./services/api";
@@ -13,13 +16,16 @@ import Renderer from "./views/Renderer/Renderer";
 
 function App() {
   const [builderFormSchema, setBuilderFormSchema] = useState<
-    {
-      id: string;
-      data: BuilderFormData[];
-    }[]
-  >([]);
+    | {
+        id: string;
+        data: BuilderFormData[];
+      }[]
+    | null
+  >(null);
   const [viewMode, setViewMode] = useState<"builder" | "preview">("builder");
   const [isLoading, setIsLoading] = useState(true);
+  const [initialDraftEntry, setInitialDraftEntry] =
+    useState<BuilderFormData | null>(null);
 
   const toggleViewMode = useCallback(() => {
     setViewMode((prev) => (prev === "builder" ? "preview" : "builder"));
@@ -33,19 +39,23 @@ function App() {
       questionId?: number,
     ) => {
       try {
+        // Update case
         if (schemaId && questionId) {
           const updatedData = await updateSchema(data, schemaId, questionId);
           if (!isNewEntry) {
             setBuilderFormSchema(updatedData);
           }
-
           return;
         }
-        await addSchema([data], schemaId);
-        if (!isNewEntry) {
-          const updatedData = await getSchemas();
-          setBuilderFormSchema(updatedData);
+        // New entry case
+        if (isNewEntry) {
+          await createDraftEntry(data);
+          return;
         }
+
+        const updatedData = await getSchemas();
+        await deleteDraftEntry();
+        setBuilderFormSchema(updatedData);
       } catch (e) {
         // TODO : Error handling
       }
@@ -53,11 +63,15 @@ function App() {
     [],
   );
 
-  const handleBuilderFormSubmit = useCallback(async () => {
-    const data = await getSchemas();
-    setIsLoading(false);
-    setBuilderFormSchema(data);
-  }, []);
+  const handleBuilderFormSubmit = useCallback(
+    async (data: BuilderFormData, schemaId?: string) => {
+      await addSchema([data], schemaId);
+      const promises = await Promise.all([getSchemas(), deleteDraftEntry()]);
+      setBuilderFormSchema(promises[0]);
+      setInitialDraftEntry(null);
+    },
+    [],
+  );
 
   const handleQuestionRemove = useCallback(
     async (schemaId: string, questionId: number) => {
@@ -69,13 +83,17 @@ function App() {
   );
 
   useEffect(() => {
-    getSchemas()
-      .then((schemas) => {
+    const promises = Promise.all([getSchemas(), getDraftEntry()]);
+
+    promises
+      .then(([schemas, draftEntry]) => {
         setBuilderFormSchema(schemas);
+        setInitialDraftEntry(draftEntry);
         setIsLoading(false);
       })
       .catch(() => {
         // TODO : Error handling
+        console.log("Error fetching data");
       });
   }, []);
 
@@ -91,6 +109,7 @@ function App() {
         size={SIZE.md}
         onClick={toggleViewMode}
         className="self-end"
+        disabled={!builderFormSchema}
       >
         Switch to {viewMode === "builder" ? "Preview" : "Builder"}
       </Button>
@@ -98,13 +117,14 @@ function App() {
         <Builder
           onQuestionRemove={handleQuestionRemove}
           // We as of now only support one form
-          builderFormData={builderFormSchema[0].data}
+          builderFormData={builderFormSchema?.[0].data ?? []}
           onSubmit={handleBuilderFormSubmit}
-          schemaId={builderFormSchema[0].id}
+          schemaId={builderFormSchema?.[0].id}
           onChange={handleBuilderFormDataChange}
+          draftEntry={initialDraftEntry}
         />
       ) : (
-        <Renderer schema={builderFormSchema[0].data} />
+        <Renderer schema={builderFormSchema?.[0].data!} />
       )}
     </main>
   );
